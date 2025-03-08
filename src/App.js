bb6d8c3dc5507b18bf2eb30241f9994d063aab63
ducanh005 <vuducanh039@gmail.com>
2025-03-08 14:00:27 +0700

    Dung khung Layout mac dinh

--- src/App.js
+++ src/App.js
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import { Fragment } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { publicRoutes } from './routes';
@@ -58,7 +35,7 @@ function App() {
             </div>
         </Router>
     );
->>>>>>> ef50c9c (cai dat va cau hinh router)
+
 }
 
 export default App;
