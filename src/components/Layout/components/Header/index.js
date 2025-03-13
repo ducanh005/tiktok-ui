c5a8829d06d4706607bbedea067c2643d2344cdc
ducanh005 <vuducanh039@gmail.com>
2025-03-14 00:14:56 +0700

    Xay dung UI header #7

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -27,6 +27,8 @@ import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/Accountitem';
 import Menu from '~/components/Popper/Menu';
+import { UploadIcon, MessageIcon, InboxIcon } from '~/components/Icons';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
@@ -80,16 +82,18 @@ function Header() {
             default:
         }
     };
-    const userMenu=[
+    const userMenu = [
         {
             icon: <FontAwesomeIcon icon={faUser} />,
             title: 'View profile',
             to: '/@Danh',
-        },{
+        },
+        {
             icon: <FontAwesomeIcon icon={faCoins} />,
             title: 'Get coins',
             to: '/coin',
-        },{
+        },
+        {
             icon: <FontAwesomeIcon icon={faGear} />,
             title: 'Setting',
             to: '/settings',
@@ -99,9 +103,9 @@ function Header() {
             icon: <FontAwesomeIcon icon={faSignOut} />,
             title: 'Log out',
             to: '/logout',
-            separate:true,
+            separate: true,
         },
-    ]
+    ];
 
     return (
         <header className={cx('wrapper')}>
@@ -139,7 +143,17 @@ function Header() {
                         <>
                             <Tippy delay={[0, 200]} content="Upload video" placement="bottom">
                                 <button className={cx('action-btn')}>
-                                    <FontAwesomeIcon icon={faCloudUpload} />
+                                    <UploadIcon></UploadIcon>
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 200]} content="Message" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <MessageIcon></MessageIcon>
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 200]} content="Inbox" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <InboxIcon></InboxIcon>
                                 </button>
                             </Tippy>
                         </>
@@ -151,11 +165,12 @@ function Header() {
                     )}
                     <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
                         {currentUser ? (
-                            <img
+                            <Image
                                 src="https://scontent.fsgn5-14.fna.fbcdn.net/v/t39.30808-1/480687451_1037736148387537_230747523904068265_n.jpg?stp=c27.105.1500.1500a_cp0_dst-jpg_s40x40_tt6&_nc_cat=1&ccb=1-7&_nc_sid=2d3e12&_nc_eui2=AeHC341Yiv1Zymf2DCbbbe4CxCCw9qFB9_bEILD2oUH39uZgMF4rsrfmS-FpTAgEQpuMcVny0RW-el76dIJXKJno&_nc_ohc=GUQQpf2q6tkQ7kNvgEkcwsB&_nc_oc=AdjVD0l4WAoVxxuQgK_76z3n3X7zpKGTvE__dfZGSt29fRFBJDBY1p6eZKwsLwym5QqiZ8Z55A-VN45Fejs52sKg&_nc_zt=24&_nc_ht=scontent.fsgn5-14.fna&_nc_gid=APA9A2oggpaGda3BJnnIzTY&oh=00_AYEVkFUtFOI086N-ulOsPgGadqMzvv4GnD1tLr9ENsrfew&oe=67D8C390"
                                 className={cx('user-avatar')}
                                 alt="Vu Duc Anh"
-                            ></img>
+                                fallback="https://scontent.fsgn5-14.fna.fbcdn.net/v/t1.30497-1/453178253_471506465671661_2781666950760530985_n.png?stp=cp0_dst-png_s40x40&_nc_cat=1&ccb=1-7&_nc_sid=136b72&_nc_eui2=AeGRDrT7zbuTK43IGXOEifs1Wt9TLzuBU1Ba31MvO4FTUC8WSJhQzesBEyBF725czsTFZMH1qBmo6-X4oIUOzymP&_nc_ohc=wOXUzZhUIhAQ7kNvgG3TuY2&_nc_oc=AdieiwO_IxX61vPLpuufoXydKxD0tISogR8b6ZeAQEEIEo65a4Lq4I1efBoegCkxtX4ZGCMtMQkIz8_H1IFcP5Yg&_nc_zt=24&_nc_ht=scontent.fsgn5-14.fna&_nc_gid=AG9w-HHq5WnJoqFve_yhsKG&oh=00_AYExPWTsPmO4Q6ysAqoUIi-KrIO3LQ2HDJoLedWt9giwDg&oe=67FA627A"
+                            />
                         ) : (
                             <button className={cx('more-btn')}>
                                 <FontAwesomeIcon icon={faEllipsisVertical} />
