d8e408f4bb58644bb3c2c5d80f8d63e50a7a7219
ducanh005 <vuducanh039@gmail.com>
2025-03-16 00:04:52 +0700

    Tim hieu thu vien Axios

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -2,48 +2,52 @@ import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { Wrapper as PropperWrapper } from '~/components/Popper';
 import classNames from 'classnames/bind';
+
+import * as searchServices from '~/apiServices/searchServices';
 import AccountItem from '~/components/Accountitem';
 import { SearchIcon } from '~/components/Icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Search.module.scss';
+import { useDebounce } from '~/hooks';
 import { useEffect, useState, useRef } from 'react';
 
 const cx = classNames.bind(styles);
 function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
-    const [showResult,setShowResult] = useState(true)
-    const [loading,setLoading] = useState(false)
+    const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    const debounced = useDebounce(searchValue, 500);
 
     const inputRef = useRef();
 
     useEffect(() => {
-        if(!searchValue.trim()){
-            setSearchResult([])
+        if (!debounced.trim()) {
+            setSearchResult([]);
             return;
         }
-        setLoading(true)
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
-        .then(res=>res.json())
-        .then(res => {
-            setSearchResult(res.data)
-            setLoading(false)
-        })
-        .catch(()=>{
-            setLoading(false)
-        })
-        
-    }, [searchValue]);
+        setLoading(true);
+
+        const fetchApi = async () => {
+            setLoading(true);
+            const result = await searchServices.search(debounced);
+            setSearchResult(result);
+            setLoading(false);
+        };
+
+        fetchApi();
+    }, [debounced]);
 
     const handleclear = () => {
         setSearchValue('');
-        setSearchResult([])
+        setSearchResult([]);
         inputRef.current.focus();
     };
 
-    const handleHideResult=()=>{
-        setShowResult(false)
-    }
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
 
     return (
         <HeadlessTippy
@@ -53,9 +57,9 @@ function Search() {
                 <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                     <PropperWrapper>
                         <h4 className={cx('search-title')}>Accounts</h4>
-                       {searchResult.map((result)=>(
-                        <AccountItem key={result.id} data={result}></AccountItem>
-                       ))}
+                        {searchResult.map((result) => (
+                            <AccountItem key={result.id} data={result}></AccountItem>
+                        ))}
                     </PropperWrapper>
                 </div>
             )}
@@ -68,9 +72,9 @@ function Search() {
                     placeholder="Search accounts and videos"
                     spellCheck={false}
                     onChange={(e) => setSearchValue(e.target.value)}
-                    onFocus={()=>setShowResult(true)}
+                    onFocus={() => setShowResult(true)}
                 ></input>
-                {!!searchValue &&!loading && (
+                {!!searchValue && !loading && (
                     <button className={cx('clear')} onClick={handleclear}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
