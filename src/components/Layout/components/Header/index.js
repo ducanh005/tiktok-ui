0b36bfb337a5e0c3ede5bf508fb045bba4848e27
ducanh005 <vuducanh039@gmail.com>
2025-03-16 21:11:30 +0700

    fix header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -14,6 +14,7 @@ import {
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 
+import routesConfig from '~/config/routes'
 import Button from '~/components/Button';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -21,6 +22,7 @@ import Menu from '~/components/Popper/Menu';
 import { UploadIcon, MessageIcon, InboxIcon } from '~/components/Icons';
 import Image from '~/components/Image';
 import Search from '../Search';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -95,7 +97,9 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
+            <Link to={routesConfig.home} className={cx('logo-link')}>
                 <img src={images.logo} alt="tik tok"></img>
+            </Link>
                 <Search></Search>
                 <div className={cx('action')}>
                     {currentUser ? (
